bd67d612a0a1b8374dc0fee3cbd3f039bad75a0c
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-09-17 14:58:14 +0400

    [New Feature] Added Shipping Bin and Items
    + Added new item class to better simulate inventory behaviour
    + Added shipping bin to manage selling of items

--- assets/scripts/garden-planner/classes/shipping-bin.ts
+++ assets/scripts/garden-planner/classes/shipping-bin.ts
@@ -3,6 +3,47 @@
  * - This class will be used in the future to store data about items being sold
  */
 
-class ShippingBin {
+import type { Item } from './item'
 
+export interface IBinInfo {
+  [day: string]: Record<string, Item>
+}
+
+export class ShippingBin {
+  private _binInfo: IBinInfo = {}
+
+  sortBin() {
+    const sortedBin: IBinInfo = {}
+    Object.keys(this._binInfo).sort((a, b) => {
+      const dayA = Number.parseInt(a)
+      const dayB = Number.parseInt(b)
+      if (dayA < dayB)
+        return -1
+      if (dayA > dayB)
+        return 1
+      return 0
+    }).forEach((key) => {
+      sortedBin[key] = this._binInfo[key]
+    })
+    this._binInfo = sortedBin
+  }
+
+  get binInfo(): IBinInfo {
+    return this._binInfo
+  }
+
+  add(day: string, item: Item): void {
+    /**
+     * I've considered making it so that the player is limited in how much they
+     *  can sell in a day based on item stack size (as there's a limited amount of slots)
+     *  but I think it's better to just let the player figure out how to manage their shipping bin
+     */
+
+    if (!this._binInfo[day])
+      this._binInfo[day] = {}
+    if (!this._binInfo[day][item.name])
+      this._binInfo[day][item.name] = item
+    else
+      this._binInfo[day][item.name].add(item.count)
+  }
 }
