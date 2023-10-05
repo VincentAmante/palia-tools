d75e9df799cbce8887dc3645a0639409932aeef7
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-10-05 22:06:12 +0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,8 @@ useHead({
 </script>
 
 <template>
-  <div class="flex flex-col max-w-[1680px] mx-auto">
-    <div class="-z-10 bg-gradient fixed w-full h-full" />
+  <div class="-z-10 bg-gradient fixed w-full h-full" />
+  <div class="flex flex-col max-w-[1440px] mx-auto">
     <PGPHeader />
     <NuxtPage />
     <PGPFooter />
