6bc726da2201141d134754d8d252761450afb2f1
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-11-11 17:42:58 +0400

    Update housing.vue

--- layouts/housing.vue
+++ layouts/housing.vue
@@ -2,29 +2,71 @@
 import HousingHeader from '~/components/house-planner/HousingHeader.vue'
 
 useHead({
-  title: 'Palia Housing Planner',
+  title: 'Palia House Planner',
   link: [
     {
       rel: 'icon',
       href: '/housing-logo.webp',
     },
   ],
-})
-
-useServerSeoMeta({
-  ogTitle: 'Palia Housing Planner',
-  ogDescription: 'Alpha release of a Palia Housing Planner, from the creator of the Garden Planner',
-  ogImage: 'https://palia-garden-planner.vercel.app/housing-logo.webp',
-  ogUrl: 'https://palia-garden-planner.vercel.app/housing',
-  ogType: 'website',
-  ogLocale: 'en_UK',
-  title: 'Palia Garden Planner',
-  description: 'Alpha release of a Palia Housing Planner, from the creator of the Garden Planner',
-  themeColor: '#B2BEFE',
-  twitterTitle: 'Palia Housing Planner',
-  twitterCard: 'summary',
-  twitterDescription: 'Alpha release of a Palia Housing Planner, from the creator of the Garden Planner',
-  twitterImage: 'https://palia-garden-planner.vercel.app/housing-logo.webp',
+  meta: [
+    {
+      name: 'description',
+      content: 'Alpha release of a Palia Housing Planner, from the creator of the Garden Planner',
+    },
+    {
+      name: 'theme-color',
+      content: '#B2BEFE',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Palia Housing Planner',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Alpha release of a Palia Housing Planner, from the creator of the Garden Planner',
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://palia-garden-planner.vercel.app/housing-logo.webp',
+    },
+    {
+      name: 'twitter:image:alt',
+      content: 'Palia Housing Planner Logo',
+    },
+    {
+      name: 'og:title',
+      content: 'Palia Housing Planner',
+    },
+    {
+      name: 'og:description',
+      content: 'Alpha release of a Palia Housing Planner, from the creator of the Garden Planner',
+    },
+    {
+      name: 'og:image',
+      content: 'https://palia-garden-planner.vercel.app/housing-logo.webp',
+    },
+    {
+      name: 'og:image:alt',
+      content: 'Palia Housing Planner Logo',
+    },
+    {
+      name: 'og:url',
+      content: 'https://palia-garden-planner.vercel.app/housing',
+    },
+    {
+      name: 'og:type',
+      content: 'website',
+    },
+    {
+      name: 'og:locale',
+      content: 'en_UK',
+    },
+  ],
 })
 </script>
 
