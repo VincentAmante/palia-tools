0091220b6e0f0b6989a424aa7398a728e75b1a8f
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2024-06-17 19:53:39 +0400

    Update plotStat.ts
    - Improved the typing of the PlotStat to cut down menial effort of having to add new crops/fertilisers

--- assets/scripts/garden-planner/types/plotStat.ts
+++ assets/scripts/garden-planner/types/plotStat.ts
@@ -4,34 +4,9 @@ import type Bonus from '../enums/bonus'
 
 interface PlotStat {
   cropCount: number
-  cropTypeCount: {
-    [CropType.Tomato]: number
-    [CropType.Potato]: number
-    [CropType.Wheat]: number
-    [CropType.Rice]: number
-    [CropType.Carrot]: number
-    [CropType.Onion]: number
-    [CropType.Cotton]: number
-    [CropType.Apple]: number
-    [CropType.Blueberry]: number
-    [CropType.None]: number
-  }
-  cropBonusCoverage: {
-    [Bonus.None]: number
-    [Bonus.HarvestIncrease]: number
-    [Bonus.WeedPrevention]: number
-    [Bonus.WaterRetain]: number
-    [Bonus.QualityIncrease]: number
-    [Bonus.SpeedIncrease]: number
-  }
-  fertiliserCount: {
-    [FertiliserType.None]: number
-    [FertiliserType.QualityUp]: number
-    [FertiliserType.HarvestBoost]: number
-    [FertiliserType.WeedBlock]: number
-    [FertiliserType.SpeedyGro]: number
-    [FertiliserType.HydratePro]: number
-  }
+  cropTypeCount: { [key in CropType]: number }
+  cropBonusCoverage: { [key in Bonus]: number }
+  fertiliserCount: { [key in FertiliserType]: number }
 }
 
 export type { PlotStat }
