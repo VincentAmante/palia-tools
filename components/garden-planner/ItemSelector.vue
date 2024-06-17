ea7c874803f32aa5be7cc54ee520f40d402bae3b
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2024-06-17 19:57:13 +0400

    Update ItemSelector.vue

--- components/garden-planner/ItemSelector.vue
+++ components/garden-planner/ItemSelector.vue
@@ -124,7 +124,7 @@ const { pause: pauseRight, resume: resumeRight } = useIntervalFn(() => {
             </li>
           </ul>
         </div>
-        <div class="flex flex-wrap gap-1 p-1 rounded-md w-fit">
+        <div class="flex gap-1 p-1 rounded-md w-fit">
           <div class="hidden pb-1 sm:flex lg:items-center">
             <button
               id="crop-eraser" aria-label="Select Crop Eraser"
