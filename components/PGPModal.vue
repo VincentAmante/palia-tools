10ff5d290ccead8fe4adf591495d1b7aefdcb213
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2025-07-06 11:36:28 +0400

    UI tweak
    ~ adjusted divider to quit jumping around

--- components/PGPModal.vue
+++ components/PGPModal.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const modal = ref<HTMLDialogElement | null>(null)
 
-  defineProps({
-    useFullHeight: {
-      type: Boolean,
-      required: false,
-      default: false,
-    }
-  })
+defineProps({
+  useFullHeight: {
+    type: Boolean,
+    required: false,
+    default: false,
+  }
+})
 
 function showModal() {
   modal.value?.showModal()
@@ -24,17 +24,19 @@ defineExpose({
 <template>
   <dialog ref="modal" class="modal py-2">
     <div class="flex flex-col gap-2 px-3 py-6 modal-box" :class="{ 'h-full': useFullHeight }">
-      <div class="flex justify-between">
-        <h2 class="text-xl ">
-          <slot name="header" />
-        </h2>
-        <form method="dialog" class="">
-          <button aria-label="close modal" class="btn btn-square btn-ghost btn-sm">
-            <font-awesome-icon :icon="['fas', 'x']" class="text-xl" />
-          </button>
-        </form>
+      <div>
+        <div class="flex justify-between">
+          <h2 class="text-xl ">
+            <slot name="header" />
+          </h2>
+          <form method="dialog" class="">
+            <button aria-label="close modal" class="btn btn-square btn-ghost btn-sm">
+              <font-awesome-icon :icon="['fas', 'x']" class="text-xl" />
+            </button>
+          </form>
+        </div>
+        <div class="my-0 divider" />
       </div>
-      <div class="my-0 divider" />
       <div class="flex flex-col gap-2 modal-body">
         <slot name="body" />
       </div>
