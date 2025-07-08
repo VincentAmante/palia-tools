cf2f063229fbc824c7406148fbc44c93532278a9
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2025-07-08 23:29:02 +0400

    Update cropList.ts

--- assets/scripts/garden-planner/cropList.ts
+++ assets/scripts/garden-planner/cropList.ts
@@ -520,10 +520,10 @@ const BATTERFLY_BEAN = new Crop(
     preserveStar: 61,
   },
   {
-    cropsPerSeed: 2,
+    cropsPerSeed: 1,
     seedsPerConversion: 1,
     cropsPerPreserve: 1,
-    seedProcessMinutes: 85,
+    seedProcessMinutes: 120,
     preserveProcessMinutes: 33,
   },
   {
